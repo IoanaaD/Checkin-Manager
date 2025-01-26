a04cea846c032d172651c1c5da9120bd46e05054
IDUMI01 <Ioana.Dumitrescu@albertsons.com>
2025-01-26 17:38:06 +0200

    setup react app drom scratch

--- src/schemas/UserSubscriptionSchemas.ts
+++ src/schemas/UserSubscriptionSchemas.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 export const userSubscriptionSchema = z.object({
     userId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid ObjectId"), 
     subscriptionPlanId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid ObjectId"),
-    startDate: z.string().refine((date) => !isNaN(Date.parse(date)), "Invalid date format"), 
-    endDate: z.string().refine((date) => !isNaN(Date.parse(date)), "Invalid date format"), 
+    startDate: z.string().refine((date: string) => !isNaN(Date.parse(date)), "Invalid date format"), 
+    endDate: z.string().refine((date: string) => !isNaN(Date.parse(date)), "Invalid date format"), 
     checkInDates: z.array(
-      z.string().refine((date) => !isNaN(Date.parse(date)), "Invalid date format") 
+      z.string().refine((date: string) => !isNaN(Date.parse(date)), "Invalid date format") 
     ).optional(),
   });
 
---
+++ web-app/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <h1>
+            Created A React App From Scratch! x
+        </h1>
+    )
+}
+
+export default App
